d3bd2d85d65cd454ac3daf033d19984e6ccf5df5
muallimim <muallimim@yahoo.com>
2022-12-31 22:04:33 +0300

    0-10 arası sayma ve mobil uyumluluk eklendi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 function increase(){
     var number;
      number = document.getElementById('count').value;
+
+     if(number<11)
      number= parseInt(number)  + 1;
+
+     if(number==11)
+     number = 0;
+
+
      document.getElementById('count').value = number;
  }
 
@@ -9,6 +16,12 @@ function increase(){
  function decrease(){
     var number;
      number = document.getElementById('count').value;
+
+     if(number>-1)
      number= parseInt(number)  - 1;
+
+     if(number==-1)
+     number=10;
+
      document.getElementById('count').value = number;
  }
